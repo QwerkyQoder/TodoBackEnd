01a93bc9bddf5eaf0f66bbe20e9a7aff8bfdee63
Meera Murthy <colorcodedcrayons@gmail.com>
2023-03-04 16:29:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`App is running on Port ${PORT}`);
 })
+
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
